e7789962d179b4e7c586ecdf3d41af5eaf35e8df
Yao Wang <yao@Yaos-Mac-mini.local>
2020-12-12 03:03:34 -0500

    search bar item styling

--- components/reserve/mapbox/SearchBar.js
+++ components/reserve/mapbox/SearchBar.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react';
 
-import { Box, TextField } from '@material-ui/core';
+import {
+    Box,
+    TextField,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { getPlaceSuggests, getPlace } from './mbx-config';
 import { MapContext } from './Map';
+import PlaceIcon from '@material-ui/icons/Public';
+import AddressIcon from '@material-ui/icons/LocationOn';
+import AirportIcon from '@material-ui/icons/Flight';
+import { PlaceTwoTone } from '@material-ui/icons';
 
 // TODO: search bar logic
 const useOptions = () => {
@@ -21,10 +31,28 @@ const useOptions = () => {
                     map.getCenter()
                 );
                 setOptions(
-                    data?.features?.map(({ place_name, place_types }) => ({
-                        place_name,
-                        place_types,
-                    })) ?? []
+                    data?.features?.map(
+                        ({ place_name, center, place_type, properties }) => {
+                            switch (place_type[0]) {
+                                case 'address':
+                                    place_type = 'address';
+                                    break;
+                                case 'poi': {
+                                    if (properties.category === 'airport')
+                                        place_type = 'airport';
+                                    else place_type = 'place';
+                                    break;
+                                }
+                                default:
+                                    place_type = 'place'; // country, region, post, neighberhood...
+                            }
+                            return {
+                                place_name,
+                                center: { lng: center[0], lat: center[1] },
+                                place_type,
+                            };
+                        }
+                    ) ?? []
                 );
             };
 
@@ -57,6 +85,31 @@ export default function SearchBar() {
             renderInput={(params) => (
                 <TextField {...params} label='Combo box' variant='outlined' />
             )}
+            renderOption={(option) => {
+                //const {type, addressString, osmPlaceId, details, source, geoSrc, primaryPart, secondaryPart} = option;
+                return <OptionItem {...option} />;
+            }}
         />
     );
 }
+
+const OptionItem = ({ place_type, place_name }) => {
+    const splitIndex = place_name.indexOf(',');
+    let primaryPart = place_name;
+    let secondaryPart = '';
+    if (splitIndex !== -1) {
+        primaryPart = place_name.slice(0, splitIndex);
+        secondaryPart = place_name.slice(splitIndex + 1);
+    }
+    return (
+        <ListItem button>
+            <ListItemIcon>
+                {(/address/i.test(place_type) && <AddressIcon />) ||
+                    (/airport/i.test(place_type) && <AirportIcon />) || (
+                        <PlaceIcon />
+                    )}
+            </ListItemIcon>
+            <ListItemText primary={primaryPart} secondary={secondaryPart} />
+        </ListItem>
+    );
+};
